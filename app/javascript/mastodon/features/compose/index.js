3e2eddcfe404f29de8b2122da6ad0375418f40a6
aquarla <satosamu@gmail.com>
2020-01-17 17:48:13 +0900

    Use fa-cutlery icon at gochisou_photo tag timeline link.

--- app/javascript/mastodon/features/compose/index.js
+++ app/javascript/mastodon/features/compose/index.js
@@ -111,7 +111,7 @@ class Compose extends React.PureComponent {
           {!columns.some(column => column.get('id') === 'PUBLIC') && (
             <Link to='/timelines/public' className='drawer__tab' title={intl.formatMessage(messages.public)} aria-label={intl.formatMessage(messages.public)}><Icon id='globe' fixedWidth /></Link>
           )}
-	  <Link to='/timelines/tag/gochisou_photo' className='drawer__tab' title={intl.formatMessage(messages.gochisou)} aria-label={intl.formatMessage(messages.gochisou)}><Icon id='hashtag' fixedWidth /></Link>
+	  <Link to='/timelines/tag/gochisou_photo' className='drawer__tab' title={intl.formatMessage(messages.gochisou)} aria-label={intl.formatMessage(messages.gochisou)}><Icon id='cutlery' fixedWidth /></Link>
           <a href='/settings/preferences' className='drawer__tab' title={intl.formatMessage(messages.preferences)} aria-label={intl.formatMessage(messages.preferences)}><Icon id='cog' fixedWidth /></a>
           <a href='/auth/sign_out' className='drawer__tab' title={intl.formatMessage(messages.logout)} aria-label={intl.formatMessage(messages.logout)} onClick={this.handleLogoutClick}><Icon id='sign-out' fixedWidth /></a>
         </nav>
