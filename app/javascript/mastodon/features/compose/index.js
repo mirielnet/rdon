c8428ef0f8516f4c04775730e57e7f5bdfc70c4e
aquarla <satosamu@gmail.com>
2020-01-17 17:22:34 +0900

    Add gochisou_photo hashtag timeline link to tab bar.

--- app/javascript/mastodon/features/compose/index.js
+++ app/javascript/mastodon/features/compose/index.js
@@ -24,6 +24,7 @@ const messages = defineMessages({
   notifications: { id: 'tabs_bar.notifications', defaultMessage: 'Notifications' },
   public: { id: 'navigation_bar.public_timeline', defaultMessage: 'Federated timeline' },
   community: { id: 'navigation_bar.community_timeline', defaultMessage: 'Local timeline' },
+  gochisou: { id: 'navigation_bar.gochisou_timeline', defaultMessage: 'Gochisou timeline' },
   preferences: { id: 'navigation_bar.preferences', defaultMessage: 'Preferences' },
   logout: { id: 'navigation_bar.logout', defaultMessage: 'Logout' },
   compose: { id: 'navigation_bar.compose', defaultMessage: 'Compose new toot' },
@@ -110,6 +111,7 @@ class Compose extends React.PureComponent {
           {!columns.some(column => column.get('id') === 'PUBLIC') && (
             <Link to='/timelines/public' className='drawer__tab' title={intl.formatMessage(messages.public)} aria-label={intl.formatMessage(messages.public)}><Icon id='globe' fixedWidth /></Link>
           )}
+	  <Link to='/timelines/tag/gochisou_photo' className='drawer__tab' title={intl.formatMessage(messages.gochisou)} aria-label={intl.formatMessage(messages.gochisou)}><Icon id='hashtag' fixedWidth /></Link>
           <a href='/settings/preferences' className='drawer__tab' title={intl.formatMessage(messages.preferences)} aria-label={intl.formatMessage(messages.preferences)}><Icon id='cog' fixedWidth /></a>
           <a href='/auth/sign_out' className='drawer__tab' title={intl.formatMessage(messages.logout)} aria-label={intl.formatMessage(messages.logout)} onClick={this.handleLogoutClick}><Icon id='sign-out' fixedWidth /></a>
         </nav>
