14c228280f4936d22df4649fb313c4cc12a4d1c0
noellabo <noel.yoshiba@gmail.com>
2021-03-24 17:14:38 +0900

    Add trends columns

--- app/javascript/mastodon/features/ui/index.js
+++ app/javascript/mastodon/features/ui/index.js
@@ -57,6 +57,7 @@ import {
   GroupDirectory,
   Directory,
   FollowRecommendations,
+  Trends,
 } from './util/async-components';
 import { me } from '../../initial_state';
 import { previewState as previewMediaState } from './components/media_modal';
@@ -179,6 +180,7 @@ class SwitchingColumnsArea extends React.PureComponent {
           <WrappedRoute path='/search' component={Search} content={children} />
           <WrappedRoute path='/group_directory' component={GroupDirectory} content={children} componentParams={{ shouldUpdateScroll: this.shouldUpdateScroll }} />
           <WrappedRoute path='/directory' component={Directory} content={children} componentParams={{ shouldUpdateScroll: this.shouldUpdateScroll }} />
+          <WrappedRoute path='/trends' component={Trends} content={children} componentParams={{ shouldUpdateScroll: this.shouldUpdateScroll }} />
 
           <WrappedRoute path='/statuses/new' component={Compose} content={children} />
           <WrappedRoute path='/statuses/:statusId' exact component={Status} content={children} componentParams={{ shouldUpdateScroll: this.shouldUpdateScroll }} />
