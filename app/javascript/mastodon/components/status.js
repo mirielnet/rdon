c8f17d8f6be5abfebf38bf02936701631a388726
noellabo <noel.yoshiba@gmail.com>
2022-02-23 10:01:21 +0900

    Remove unneeded code from quote

--- app/javascript/mastodon/components/status.js
+++ app/javascript/mastodon/components/status.js
@@ -112,7 +112,6 @@ class Status extends ImmutablePureComponent {
     onHeightChange: PropTypes.func,
     onToggleHidden: PropTypes.func,
     onToggleCollapsed: PropTypes.func,
-    onQuoteToggleHidden: PropTypes.func,
     muted: PropTypes.bool,
     hidden: PropTypes.bool,
     unread: PropTypes.bool,
@@ -216,7 +215,7 @@ class Status extends ImmutablePureComponent {
   }
 
   handleExpandedQuoteToggle = () => {
-    this.props.onQuoteToggleHidden(this._properStatus());
+    this.props.onToggleHidden(this._properQuoteStatus());
   };
 
   renderLoadingMediaGallery () {
@@ -668,7 +667,7 @@ class Status extends ImmutablePureComponent {
                 <DisplayName account={quote_status.get('account')} />
               </a>
             </div>
-            <StatusContent status={quote_status} onClick={this.handleQuoteClick} expanded={!status.get('quote_hidden')} onExpandedToggle={this.handleExpandedQuoteToggle} quote />
+            <StatusContent status={quote_status} onClick={this.handleQuoteClick} expanded={!quote_status.get('hidden')} onExpandedToggle={this.handleExpandedQuoteToggle} quote />
             {quote_media}
           </div>
         );
