f541f3d1a90f6273f1b732cba869f7d7c1290807
Claire <claire.github-309c@sitedethib.com>
2023-05-17 00:08:42 +0200

    Fix being able to vote on your own polls (#25015)

--- app/validators/vote_validator.rb
+++ app/validators/vote_validator.rb
@@ -3,8 +3,8 @@
 class VoteValidator < ActiveModel::Validator
   def validate(vote)
     vote.errors.add(:base, I18n.t('polls.errors.expired')) if vote.poll_expired?
-
     vote.errors.add(:base, I18n.t('polls.errors.invalid_choice')) if invalid_choice?(vote)
+    vote.errors.add(:base, I18n.t('polls.errors.self_vote')) if self_vote?(vote)
 
     if vote.poll_multiple? && already_voted_for_same_choice_on_multiple_poll?(vote)
       vote.errors.add(:base, I18n.t('polls.errors.already_voted'))
@@ -17,6 +17,10 @@ class VoteValidator < ActiveModel::Validator
 
   def invalid_choice?(vote)
     vote.choice.negative? || vote.choice >= vote.poll.options.size
+  end
+
+  def self_vote?(vote)
+    vote.account_id == vote.poll.account_id
   end
 
   def already_voted_for_same_choice_on_multiple_poll?(vote)
