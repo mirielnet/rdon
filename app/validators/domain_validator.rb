477584705b2ce3619ce0c56fb031f73f5893c65b
noellabo <noel.yoshiba@gmail.com>
2023-07-31 08:55:41 +0900

    Fix domain validater

--- app/validators/domain_validator.rb
+++ app/validators/domain_validator.rb
@@ -2,17 +2,13 @@
 
 class DomainValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return unless value.is_a?(String)
-
-    domain = begin
-      if options[:acct]
-        value.split('@')[1]
-      else
-        value
-      end
-    end
-
     return if value.blank?
+
+    domain = if options[:acct]
+               value.split('@').last
+             else
+               value
+             end
 
     record.errors.add(attribute, I18n.t('domain_validator.invalid_domain')) unless compliant?(domain)
   end
@@ -20,6 +16,8 @@ class DomainValidator < ActiveModel::EachValidator
   private
 
   def compliant?(value)
-    value.match?(Twitter::TwitterText::Regex[:valid_domain])
+    Addressable::URI.new.tap { |uri| uri.host = value }
+  rescue Addressable::URI::InvalidURIError, IDN::Idna::IdnaError
+    false
   end
 end
