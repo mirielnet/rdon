9b6d7613e1970bf056c266acdd3ea2e530b4bf77
Claire <claire.github-309c@sitedethib.com>
2024-01-10 16:05:46 +0100

    Fix potential redirection loop of streaming endpoint (#28665)

--- app/controllers/api/v1/streaming_controller.rb
+++ app/controllers/api/v1/streaming_controller.rb
@@ -2,14 +2,19 @@
 
 class Api::V1::StreamingController < Api::BaseController
   def index
-    if Rails.configuration.x.streaming_api_base_url != request.host
-      redirect_to streaming_api_url, status: 301
-    else
+    if same_host?
       not_found
+    else
+      redirect_to streaming_api_url, status: 301
     end
   end
 
   private
+
+  def same_host?
+    base_url = Addressable::URI.parse(Rails.configuration.x.streaming_api_base_url)
+    request.host == base_url.host && request.port == (base_url.port || 80)
+  end
 
   def streaming_api_url
     Addressable::URI.parse(request.url).tap do |uri|
