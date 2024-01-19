eeeb18fde7d4f2a3cb0924b4ce46d0840f6b3a38
Claire <claire.github-309c@sitedethib.com>
2024-01-19 13:43:10 +0100

    Fix processing of compacted single-item JSON-LD collections (#28816)

--- app/services/activitypub/fetch_featured_tags_collection_service.rb
+++ app/services/activitypub/fetch_featured_tags_collection_service.rb
@@ -57,9 +57,9 @@ class ActivityPub::FetchFeaturedTagsCollectionService < BaseService
       items = begin
         case collection['type']
         when 'Collection', 'CollectionPage'
-          collection['items']
+          as_array(collection['items'])
         when 'OrderedCollection', 'OrderedCollectionPage'
-          collection['orderedItems']
+          as_array(collection['orderedItems'])
         end
       end
 
