b02a3012db813c6b669c9aab6198fd27a1abdfb5
noellabo <noel.yoshiba@gmail.com>
2024-05-01 16:03:02 +0900

    Fix post not being removed from list when muted

--- app/workers/mute_worker.rb
+++ app/workers/mute_worker.rb
@@ -5,6 +5,7 @@ class MuteWorker
 
   def perform(account_id, target_account_id)
     FeedManager.instance.clear_from_home(Account.find(account_id), Account.find(target_account_id))
+    FeedManager.instance.clear_from_lists(Account.find(account_id), Account.find(target_account_id))
   rescue ActiveRecord::RecordNotFound
     true
   end
